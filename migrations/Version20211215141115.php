fe402116a9528b08cf3526cff612544986616de4
Guillou33 <autiergilles@gmail.com>
2021-12-15 15:14:07 +0100

    get info user

--- migrations/Version20211215141115.php
+++ migrations/Version20211215141115.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20211215113917 extends AbstractMigration
+final class Version20211215141115 extends AbstractMigration
 {
     public function getDescription(): string
     {
