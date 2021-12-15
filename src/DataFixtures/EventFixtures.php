95d0b718cd2d4a220f6d3e2ceb176a441e786532
Xavier Laviron <xavier@norival.dev>
2021-12-15 11:40:20 +0100

    adds some fixtures

---
+++ src/DataFixtures/EventFixtures.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Event;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class EventFixtures extends Fixture
+{
+    public const CHRISTMAS_EVENT = "christmas";
+
+    public function load(ObjectManager $manager): void
+    {
+        $event = new Event();
+        $event->setStartDate(new \DateTime('2021-12-01'));
+        $event->setEndDate(new \DateTime('2021-12-24'));
+        $event->setTitle("Calendrier de l'avent");
+
+        $this->addReference(self::CHRISTMAS_EVENT, $event);
+        $manager->persist($event);
+
+        $manager->flush();
+    }
+}
