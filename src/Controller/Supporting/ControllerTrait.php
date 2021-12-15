94ff44e9f161c7f7fb72664546856a3695489743
Mams <mahamadou@nowledgeable.com>
2021-12-15 12:37:28 +0100

    Adding event routing

--- src/Controller/Supporting/ControllerTrait.php
+++ src/Controller/Supporting/ControllerTrait.php
@@ -9,12 +9,12 @@ trait ControllerTrait
 {
 
     public function convertTime(string $date): DateTimeInterface{
-        $newDate = DateTime::createFromFormat('d/m/Y-H:i',$date);
+        $newDate = DateTime::createFromFormat('Y-m-d H:i:s',$date);
         return $newDate;
     }
 
     public function addAndSave($entity){   
-        $em = $this->getDoctrine()->getManager();
+        $em = $this->getDoctrine()->getManager(); 
         $em->persist($entity);
         $em->flush();
     }
