584e3529b945e59d61aa1061eac09e4c26179e2a
Xavier Laviron <xavier@norival.dev>
2021-12-15 14:56:02 +0100

    adds calendar creation

--- src/Controller/CalendarController.php
+++ src/Controller/CalendarController.php
@@ -2,15 +2,23 @@
 
 namespace App\Controller;
 
+use App\Entity\Calendar;
+use App\Entity\User;
+use App\Form\CalendarType;
 use App\Repository\CalendarRepository;
+use App\Repository\EventRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CalendarController extends AbstractController
 {
     public function __construct(
         private CalendarRepository $calendarRepository,
+        private EntityManagerInterface $em,
+        private EventRepository $eventRepository,
     ) { }
 
     #[Route('/api/calendar', name: 'calendar')]
@@ -25,6 +33,37 @@ class CalendarController extends AbstractController
             context: [
                 'groups' => ['calendar_list'],
             ]
+        );
+    }
+
+    #[Route('/api/calendar/new', name: 'calendar_new')]
+    #[IsGranted('ROLE_USER')]
+    public function new(
+        #[CurrentUser] ?User $user,
+        Request $request,
+    ): JsonResponse {
+        $data = \json_decode($request->getContent(), true);
+
+        $calendar = new Calendar();
+        $form = $this->createForm(CalendarType::class, $calendar);
+
+        $form->submit($data);
+        if ($form->isValid()) {
+            $calendar->setUser($user);
+
+            $this->em->persist($calendar);
+            $this->em->flush();
+
+            return $this->json([
+                'message' => 'ok',
+            ], 200);
+        }
+
+        return $this->json(
+            [
+                'message' => (string) $form->getErrors(true, true),
+            ],
+            422
         );
     }
 }
