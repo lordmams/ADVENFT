af8194be768055534a8fe4d14914f1356dfa88c3
Guillou33 <autiergilles@gmail.com>
2021-12-15 14:11:25 +0100

    get info user

--- src/Controller/User/UserController.php
+++ src/Controller/User/UserController.php
@@ -5,6 +5,7 @@ namespace App\Controller\User;
 use App\Entity\Calendar;
 use App\Entity\Event;
 use App\Entity\User;
+use App\Controller\Supporting\ControllerTrait;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,6 +13,8 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
 {
+    use ControllerTrait;
+
     #[Route('/api/user', name: 'api_user', methods: ['GET'])]
     public function index(Request $request): Response
     {
@@ -25,80 +28,25 @@ class UserController extends AbstractController
         // see "Authorization" below
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
 
-        // returns your User object, or null if the user is not authenticated
-        // use inline documentation to tell your editor your exact User class
-        /** @var \App\Entity\User $user */
-        $user = $this->getUser();
+//        // returns your User object, or null if the user is not authenticated
+//        // use inline documentation to tell your editor your exact User class
+//        /** @var \App\Entity\User $user */
+//        $user = $this->getUser();
 
         // Call whatever methods you've added to your User class
         // For example, if you added a getFirstName() method, you can use that.
         return $this->json(['data' => $user], $status = 200, $headers = []);
     }
 
-//    #[Route('/api/user/calendar', name: 'api_user_donation', methods: ['POST'])]
-//    public function userCalendar(Request $request): Response
-//    {
-//        $body = $request->toArray();
-//
-//        $eventId = $body["eventId"];
-//        $calendarTitle = $body["calendarTitle"];
-//        $hasDonation = $body["hasDonation"];
-//
-//        $tokenUser = $request->headers->get('Authorization');
-//
-//        $user = $this->getDoctrine()->getRepository(User::class)->findOneBy([
-//            'apiToken' => $tokenUser
-//        ]);
-//
-//        $event = $this->getDoctrine()->getRepository(Event::class)->find($eventId);
-//
-//        $calendar = new Calendar();
-//        $calendar->setTitle($calendarTitle);
-//        $calendar->setEvent($event);
-//        $calendar->setHasDonation($hasDonation);
-//
-//        $entityManager = $this->getDoctrine()->getManager();
-//        $entityManager->persist($calendar);
-//
-//        $user->setCalendar($calendar);
-//        $entityManager->persist($user);
-//
-//        $entityManager->flush();
-//
-//        return $this->json(['data' => $calendar], $status = 200, $headers = []);
-//    }
-//
-//    #[Route('/api/user/donation', name: 'api_user_donation', methods: ['POST'])]
-//    public function userDonation(Request $request): Response
-//    {
-//        $body = $request->toArray();
-//
-//        $amount = $body["amount"];
-//        $calendarTitle = $body["calendarTitle"];
-//        $hasDonation = $body["hasDonation"];
-//
-//        $tokenUser = $request->headers->get('Authorization');
-//
-//        $user = $this->getDoctrine()->getRepository(User::class)->findOneBy([
-//            'apiToken' => $tokenUser
-//        ]);
-//
-//        $event = $this->getDoctrine()->getRepository(Event::class)->find($eventId);
-//
-//        $calendar = new Calendar();
-//        $calendar->setTitle($calendarTitle);
-//        $calendar->setEvent($event);
-//        $calendar->setHasDonation($hasDonation);
-//
-//        $entityManager = $this->getDoctrine()->getManager();
-//        $entityManager->persist($calendar);
-//
-//        $user->setCalendar($calendar);
-//        $entityManager->persist($user);
-//
-//        $entityManager->flush();
-//
-//        return $this->json(['data' => $calendar], $status = 200, $headers = []);
-//    }
+    #[Route('/api/user/{id}/delete', name: 'api_user', methods: ['DELETE'])]
+    public function deleteUser(Request $request, int $id){
+        $userId = $id;
+
+        $user = $this->getDoctrine()->getRepository(Event::class)->find($userId);
+
+        $this->deleteAndSave($user);
+
+        return $this->json(['data' => 'User ' . $userId . ' deleted!'], $status = 200, $headers = []);
+    }
 
 }
